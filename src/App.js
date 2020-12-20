542dc741bc40c7b57cff6dc097a2727a3a536b4e
rajeski <asiajsr@gmail.com>
2020-12-20 18:34:58 +0900

    Continued building App.js file structure

--- src/App.js
+++ src/App.js
@@ -46,15 +46,61 @@ function SignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
+
   return (
-    <button onClick={signinwithGoogle}>Sign in with Google</button>
+    <>
+    <button className="sign-in" onClick={signinwithGoogle}>Sign in with Google</button>
+    </>
   )
+
 }
 
 function SignOut() {
   return auth.currentUser && (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
+}
+
+function ChatRoom() {
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+}
+
+return (
+  <>
+  <div>
+    {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+  </div>
+
+<form>
+
+<input value={formValue} />
+
+<button type="submit"></button>
+
+</form>
+
+  </>
+)
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} />
+      <p>{text}</p>
+
+    </div>
+  )
+
 }
 
 export default App;
