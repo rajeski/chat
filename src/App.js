cab81a04536e520537f417b4b890e92cffdd9c90
rajeski <asiajsr@gmail.com>
2020-12-20 19:27:40 +0900

    Added Firebase functions; edited App.css; App.js; functions/index.js-files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
@@ -63,23 +63,46 @@ function SignOut() {
 
 function ChatRoom() {
 
+  const dummy = useRef()
+
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createAt').limit(25);
 
   const [messages] = useCollectionData(query, {idField: 'id'});
 
   const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue, 
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid, 
+      photoURL
+    });
+    setFormValue('');
+
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+
+  }
 }
 
 return (
   <>
-  <div>
+  <main>
     {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-  </div>
 
-<form>
+    <div ref={dummy}></div>
 
-<input value={formValue} />
+  </main>
+
+<form onSubmit={sendMessage}>
+
+<input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
 
 <button type="submit"></button>
 
